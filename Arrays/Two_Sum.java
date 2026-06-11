f6ddac9f4e067654450b2debe56848819c460413
vikas042 <vlp578012@gmail.com>
2026-06-11 14:04:45 +0530

    Add Two Sum solution

---
+++ Arrays/Two_Sum.java
@@ -0,0 +1,34 @@
+/*
+Problem: Two Sum
+LeetCode: https://leetcode.com/problems/two-sum/
+Difficulty: Easy
+
+Approach:
+Use a HashMap to store the numbers and their indices.
+For each element, calculate the complement (target - current element).
+If the complement exists in the HashMap, return the indices.
+Otherwise, store the current element and its index.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
