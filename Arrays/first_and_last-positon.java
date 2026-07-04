43601775b5b372be1163f63d88227d8ffe2c6091
vikas042 <vlp578012@gmail.com>
2026-07-04 08:51:51 +0530

    Add LeetCode 34: Find First and Last Position of Element in Sorted Array

---
+++ Arrays/first_and_last-positon.java
@@ -0,0 +1,80 @@
+public /*
+Problem: Find First and Last Position of Element in Sorted Array
+Difficulty: Medium
+
+Approach:
+
+1. Use Binary Search twice.
+2. First Binary Search finds the leftmost occurrence
+   of the target.
+3. Second Binary Search finds the rightmost occurrence
+   of the target.
+4. Return both indices.
+5. If the target is not present, return [-1, -1].
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int findLast(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+} {
+    
+}
